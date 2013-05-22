49beae331a7643f00fc57af3347389628298f7f1
Henric Andersson <henric.andersson@sonymobile.com>
2013-05-22 10:57:44 -0700

    Uses device_tablet.mk instead of device.mk
    Needs patch in device/sony/lagan
    
    Change-Id: I8a74b28d1aebf05cef0871ca4f5c7d16b6e4b065

--- full_sgp321.mk
+++ full_sgp321.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, device/sony/lagan/device.mk)
+$(call inherit-product, device/sony/lagan/device_tablet.mk)
 $(call inherit-product, vendor/sony/sgp321/sgp321-vendor.mk)
 
 PRODUCT_NAME := full_sgp321
